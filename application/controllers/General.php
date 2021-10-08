2a3a4440578baea0a36e548334aded917ef0da46
Manuchehr Usmonov <34451515+con9799@users.noreply.github.com>
2021-10-09 01:43:23 +0500

    Update General.php

--- application/controllers/General.php
+++ application/controllers/General.php
@@ -12,17 +12,4 @@ class General extends CI_Controller {
 	{
 		$this->load->view('main/error');
 	}
-
-	public function test()
-	{
-		
-		$this->db->where('approve', '1');
-		$this->db->like('title', 'q');
-		$this->db->or_like('descr', 'q');
-		$this->db->or_like('keywords', 'q');
-		$this->db->or_like('alt_name', 'q');
-		$this->db->limit(10);
-		$this->db->order_by('date', 'desc');
-		$query = $this->db->get('dle_post');
-	}
 }
